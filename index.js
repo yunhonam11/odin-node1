68af621dce8dfea77fa9fea255bf60531aae16fc
yunhonam11 <41495247+yunhonam11@users.noreply.github.com>
2021-09-15 21:30:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 8080;
-
 const server = http.createServer((req, res) => {
     if (req.url === '/')
     {
